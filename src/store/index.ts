e8eb6d5ff86acfc891a6365be15625364f68bbcd
ramfan <a.tambovtsev@infomaximum.com>
2019-04-16 11:10:07 +0300

    feat: добавил redux-form в зависимости и стор

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
+import {createStore, applyMiddleware, combineReducers} from "redux";
 import { reducers } from "./store";
 import logger from "../middlewares/logger";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { reducer as formReducer } from 'redux-form'
 
+const allReducers = {
+    reducers,
+    from: formReducer
+};
+
+const reducersCombine = combineReducers(allReducers);
 const enhancer = composeWithDevTools(applyMiddleware(logger));
-const store = createStore(reducers, {}, enhancer);
+const store = createStore(reducersCombine, {}, enhancer);
 
 export default store;
